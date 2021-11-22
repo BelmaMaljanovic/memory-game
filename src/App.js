801e648fd2b22038917b6f7bb55f5364e02c8488
Belma <bmaljanovic@gmail.com>
2021-11-22 20:17:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import { useState, useEffect, useContext } from "react";
+import "./App.css";
+import SingleCard from "./components/SingleCard";
+import { GlobalContext } from "./context/GlobalContext";
+
+const cardImages = [
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
+];
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const { choiceOne, choiceTwo, setChoiceTwo, setChoiceOne } =
+    useContext(GlobalContext);
+  //const [choiceOne, setChoiceOne] = useState(null)
+  //const [choiceTwo, setChoiceTwo] = useState(null)
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Magic match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            key={card.id}
+            card={card}
+            disabled={choiceOne && choiceTwo}
+          />
+        ))}
+      </div>
+      <h2>Turns : {turns}</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SingleCard.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import "./SingleCard.css";
+import { GlobalContext } from "../context/GlobalContext";
+
+const SingleCard = ({ card, flipped, disabled }) => {
+  const { choiceOne, setChoiceTwo, setChoiceOne } = useContext(GlobalContext);
+  const handleBackClick = () => {
+    if (!disabled) choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" alt="card front" src={card.src}></img>
+        <img
+          className="back"
+          alt="card back"
+          onClick={handleBackClick}
+          src="/img/cover.png"
+        ></img>
+      </div>
+    </div>
+  );
+};
+
+export default SingleCard;
